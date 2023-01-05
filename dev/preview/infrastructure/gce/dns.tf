c1d03e2287e0755b6d2a3faaf66ea334c71b775f
Aleksandar Aleksandrov <a.g.aleksandrov@gmail.com>
2023-01-05 09:48:41 +0000

    (☞ﾟヮﾟ)☞ ┻━┻

--- dev/preview/infrastructure/gce/dns.tf
+++ dev/preview/infrastructure/gce/dns.tf
@@ -1,50 +1,48 @@
-#data "google_dns_managed_zone" "preview-gitpod-dev" {
-#  provider = google
-#  name     = "preview-gitpod-dev-com"
-#}
-#
-#resource "google_dns_record_set" "root" {
-#  provider = google
-#
-#  name = "${var.preview_name}.${data.google_dns_managed_zone.preview-gitpod-dev.dns_name}"
-#  type = "A"
-#  ttl  = 300
-#
-#  managed_zone = data.google_dns_managed_zone.preview-gitpod-dev.name
-#  rrdatas      = [var.harvester_ingress_ip]
-#}
-#
-#resource "google_dns_record_set" "root-wc" {
-#  provider = google
-#
-#  name = "*.${var.preview_name}.${data.google_dns_managed_zone.preview-gitpod-dev.dns_name}"
-#  type = "A"
-#  ttl  = 300
-#
-#  managed_zone = data.google_dns_managed_zone.preview-gitpod-dev.name
-#  rrdatas      = [var.harvester_ingress_ip]
-#}
-#
-#
-#resource "google_dns_record_set" "root-wc-ws-dev" {
-#  provider = google
-#
-#  name = "*.ws-dev.${var.preview_name}.${data.google_dns_managed_zone.preview-gitpod-dev.dns_name}"
-#  type = "A"
-#  ttl  = 300
-#
-#  managed_zone = data.google_dns_managed_zone.preview-gitpod-dev.name
-#  rrdatas      = [var.harvester_ingress_ip]
-#}
-#
-#
-#resource "google_dns_record_set" "root-wc-ws-dev-ssh" {
-#  provider = google
-#
-#  name = "*.ssh.ws-dev.${var.preview_name}.${data.google_dns_managed_zone.preview-gitpod-dev.dns_name}"
-#  type = "A"
-#  ttl  = 300
-#
-#  managed_zone = data.google_dns_managed_zone.preview-gitpod-dev.name
-#  rrdatas      = [google_compute_instance.default.network_interface.0.access_config.0.nat_ip]
-#}
+data "google_dns_managed_zone" "preview-gitpod-dev" {
+  provider = google
+  name     = "preview-gitpod-dev-com"
+}
+
+resource "google_dns_record_set" "root" {
+  provider = google
+
+  name = "${var.preview_name}.${data.google_dns_managed_zone.preview-gitpod-dev.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = data.google_dns_managed_zone.preview-gitpod-dev.name
+  rrdatas      = [google_compute_instance.default.network_interface.0.access_config.0.nat_ip]
+}
+
+resource "google_dns_record_set" "root-wc" {
+  provider = google
+
+  name = "*.${var.preview_name}.${data.google_dns_managed_zone.preview-gitpod-dev.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = data.google_dns_managed_zone.preview-gitpod-dev.name
+  rrdatas      = [google_compute_instance.default.network_interface.0.access_config.0.nat_ip]
+}
+
+resource "google_dns_record_set" "root-wc-ws-dev" {
+  provider = google
+
+  name = "*.ws-dev.${var.preview_name}.${data.google_dns_managed_zone.preview-gitpod-dev.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = data.google_dns_managed_zone.preview-gitpod-dev.name
+  rrdatas      = [google_compute_instance.default.network_interface.0.access_config.0.nat_ip]
+}
+
+resource "google_dns_record_set" "root-wc-ws-dev-ssh" {
+  provider = google
+
+  name = "*.ssh.ws-dev.${var.preview_name}.${data.google_dns_managed_zone.preview-gitpod-dev.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = data.google_dns_managed_zone.preview-gitpod-dev.name
+  rrdatas      = [google_compute_instance.default.network_interface.0.access_config.0.nat_ip]
+}
